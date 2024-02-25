9f9396c8c1f81df419f150da7491d07a571c9676
Samuel Duarte <sduarteft@gmail.com>
2024-02-25 17:47:42 -0300

    feat: refactor simplify code

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,12 +4,11 @@ import { HttpRequest, HttpResponse } from "../protocols/http";
 
 export class SignUpController {
   handle(httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return BadRequest(new MissingParamError("name"));
-    }
-
-    if (!httpRequest.body.email) {
-      return BadRequest(new MissingParamError("email"));
+    const requiredFiels = ["name", "email"];
+    for (const field of requiredFiels) {
+      if (!httpRequest.body[field]) {
+        return BadRequest(new MissingParamError(field));
+      }
     }
   }
 }

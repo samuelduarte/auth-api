2007d5303820fff3d47df93984439a7ece578310
Samuel Duarte <sduarteft@gmail.com>
2024-02-25 17:04:55 -0300

    test: add validate when is not sent emaikl

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -4,15 +4,34 @@ describe("SignUpController", () => {
   test("should return 400 if no name is informed", () => {
     const sut = new SignUpController();
 
-    const input = {
-      email: "teste@gmail.com",
-      password: "123",
-      passwordConfirmation: "123",
+    const httpRequest = {
+      body: {
+        email: "teste@gmail.com",
+        password: "123",
+        passwordConfirmation: "123",
+      },
     };
 
-    const response = sut.handle(input);
+    const response = sut.handle(httpRequest);
 
     expect(response.statusCode).toBe(400);
     expect(response.body).toEqual(new Error("missing param: name"));
+  });
+
+  test("should return 400 if no email is informed", () => {
+    const sut = new SignUpController();
+
+    const httpRequest = {
+      body: {
+        name: "teste",
+        password: "123",
+        passwordConfirmation: "123",
+      },
+    };
+
+    const response = sut.handle(httpRequest);
+
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toEqual(new Error("missing param: email"));
   });
 });

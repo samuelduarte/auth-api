a68857b9082b95b0da7e9169dba079317a2aa25f
Samuel Duarte <sduarteft@gmail.com>
2024-03-01 11:26:41 -0300

    feat: create sut

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -2,9 +2,13 @@ import bcrypt from "bcrypt";
 import { BcryptAdapter } from "./bcrypt-adapter";
 
 describe("Bcrypt Adapter", () => {
+  const salt = 12;
+  const makeSutBcryptAdapter = (): BcryptAdapter => {
+    return new BcryptAdapter(salt);
+  };
   test("should call bcrypt", () => {
     const salt = 12;
-    const sut = new BcryptAdapter(salt);
+    const sut = makeSutBcryptAdapter();
     const bcryptSpyOn = jest.spyOn(bcrypt, "hash");
     sut.encrypt("any_value");
     expect(bcryptSpyOn).toHaveBeenCalledWith("any_value", salt);

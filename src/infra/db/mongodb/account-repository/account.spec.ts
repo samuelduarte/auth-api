ab318ff90450c10343e23d9b37a9d0385acda397
Samuel Duarte <sduarteft@gmail.com>
2024-03-02 11:52:19 -0300

    feat: create moongo Helper

---
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,26 @@
+import { MongoHelper } from "../helpers/helper-mongo";
+
+describe("Account Mongo Repository", () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL);
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnet();
+  });
+
+  test("should return success", () => {
+    const sut = new AccountMongoRepository();
+
+    const account = await sut.add({
+      name: "valid",
+      email: "valid@gmail.com",
+      password: "123",
+    });
+
+    expect(account.id).toBeTruthy();
+    expect(account.name).toBe("valid");
+    expect(account.email).toBe("valid@gmail.com");
+    expect(account.password).toBe("123");
+  });
+});
